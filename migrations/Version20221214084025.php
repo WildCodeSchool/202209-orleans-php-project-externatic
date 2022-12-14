23f71a5c086859cc11dc7450850a39021f8497bd
WILLIAM BOUET <william.bouet.14@gmail.com>
2022-12-14 15:49:37 +0100

    resolved problem

--- migrations/Version20221214084025.php
+++ migrations/Version20221214084025.php
@@ -22,7 +22,6 @@ final class Version20221214084025 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE candidate (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, nationality VARCHAR(100) NOT NULL, postal_code VARCHAR(5) NOT NULL, city VARCHAR(255) NOT NULL, address VARCHAR(255) DEFAULT NULL, hobby LONGTEXT DEFAULT NULL, about_me LONGTEXT DEFAULT NULL, github VARCHAR(255) DEFAULT NULL, linkedin VARCHAR(255) DEFAULT NULL, portfolio VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_6AB5B471A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE candidate ADD CONSTRAINT FK_6AB5B471A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE user DROP avatar');
     }
 
     public function down(Schema $schema): void
