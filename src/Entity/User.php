c0c63a0b69288c054551c40c70c6782a422fe939
hhertout <hugues.hertout@gmail.com>
2022-12-14 10:41:10 +0100

    fix translation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Doctrine\DBAL\Types\Types;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'L\'adresse mail est déjà utilisée')]
 #[Vich\Uploadable]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
