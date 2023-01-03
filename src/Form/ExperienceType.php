c120a25c363f9743de83d2453b8bb370cd98661e
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-01-03 16:02:19 +0100

    edit experience

--- src/Form/ExperienceType.php
+++ src/Form/ExperienceType.php
@@ -6,20 +6,41 @@ use App\Entity\Experience;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ExperienceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('company')
-            ->add('startDate')
-            ->add('endDate')
-            ->add('isCurrentPosition')
-            ->add('jobTitle')
-            ->add('jobDescription')
-            ->add('candidate')
-        ;
+            ->add('company', TextType::class, [
+                'label' => 'Entreprise',
+                'help' => 'Renseignez le nom de l\'entreprise',
+            ])
+            ->add('startDate', DateType::class, [
+                'label' => 'Date de début',
+            ])
+            ->add('endDate', DateType::class, [
+                'label' => 'Date de fin',
+                'required' => false,
+            ])
+            ->add('isCurrentPosition', CheckboxType::class, [
+                'label' => 'Poste actuel',
+                'required' => false,
+                'label_attr' => ["class" => "fs-5"]
+            ])
+            ->add('jobTitle', TextType::class, [
+                'label' => 'Intitulé du poste',
+                'help' => 'Renseignez le nom de l\'entreprise',
+            ])
+            ->add('jobDescription', TextareaType::class, [
+                'label' => 'Description',
+                'help' => 'Donnez une description de votre expérience',
+                'attr' => ["rows" => "3"]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
