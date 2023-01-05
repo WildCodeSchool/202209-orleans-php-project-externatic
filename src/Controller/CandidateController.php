3a4bd5d03e534c2e285fdceb6554c06c68059b78
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-01-05 11:30:59 +0100

    the apply functionality has been set in POST instead of GET

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Offer;
 use App\Entity\Candidate;
 use App\Form\CandidateType;
 use App\Repository\CandidateRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/candidat')]
 class CandidateController extends AbstractController
@@ -45,5 +46,22 @@ class CandidateController extends AbstractController
             'form' => $form,
             'user' => $candidate->getUser(),
         ]);
+    }
+
+    #[Route('/{candidate}/{offer}/candidater', name: 'app_candidate_applyToJob', methods: ['GET','POST'])]
+    public function applyToJob(
+        Request $request,
+        Candidate $candidate,
+        Offer $offer,
+        CandidateRepository $candidateRepository
+    ): Response {
+        $submittedToken = $request->request->get('token');
+
+        if ($this->isCsrfTokenValid('apply-offer', $submittedToken)) {
+            $candidate->addOffer($offer);
+            $candidateRepository->save($candidate, true);
+        }
+
+        return $this->redirectToRoute('app_offer_index', [], Response::HTTP_SEE_OTHER);
     }
 }
