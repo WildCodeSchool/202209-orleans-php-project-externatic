ac38d99d70e4ffa10e6ca50f91e58c8c69200a06
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-01-09 12:39:44 +0100

    S04US16 upload cv

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -2,17 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Entity\Offer;
 use App\Entity\Candidate;
 use App\Form\CandidateType;
 use App\Repository\CandidateRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/candidat')]
