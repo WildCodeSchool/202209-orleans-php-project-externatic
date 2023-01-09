0262ae5a76eed600460f6b92ae7602b084d5df0b
hhertout <hugues.hertout@gmail.com>
2023-01-09 12:19:05 +0100

    dashboardV2

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -1,14 +1,18 @@
 const toggleMenuButton = document.getElementById('dashboard-mobile')
 const dashboardPannel = document.getElementById('dashboard-nav-pannel')
 const dashboardNavlinks = document.querySelectorAll('.dashboard-link-title')
-const dashboardNavButtons = document.querySelectorAll('.dashboard-menu-link')
+const dashboardNavButtons = document.querySelectorAll('.menu-link')
+const groupLinksTitle = document.querySelectorAll('.links-title')
 const dashboardContent = document.getElementById('dashboard-content')
 
-
 toggleMenuButton.addEventListener('click', () => {
+
     toggleMenuButton.classList.toggle('dashboard-mobile-open')
     dashboardPannel.classList.toggle('dashboard-nav-open')
+    groupLinksTitle.forEach(links => links.classList.toggle('text-center'))
     dashboardNavlinks.forEach(links => links.classList.toggle('d-block'))
-    dashboardNavButtons.forEach(links => links.classList.toggle('p-3'))
+    dashboardNavButtons.forEach(links => links.classList.toggle('menu-link-close'))
+    dashboardNavButtons.forEach(links => links.classList.toggle('menu-link-open'))
     dashboardContent.classList.toggle('d-none')
+    
 })
