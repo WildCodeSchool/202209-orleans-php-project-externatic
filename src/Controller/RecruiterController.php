a4ae0a1f85a753b068f857e8971c854388303715
cedriclv <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-01-09 11:44:07 +0100

    split en deux pages

--- src/Controller/RecruiterController.php
+++ src/Controller/RecruiterController.php
@@ -12,10 +12,20 @@ class RecruiterController extends AbstractController
     #[Route('/recruiter', name: 'app_recruiter')]
     public function index(OfferRepository $offerRepository): Response
     {
-        $offers = $offerRepository->findAll();
+        $offers = $offerRepository->findBy([], ['createdAt' => 'DESC']);
 
         return $this->render('recruiter/index.html.twig', [
             'offers' => $offers,
+        ]);
+    }
+
+    #[Route('/recruiter/applications/{id}', name: 'app_recruiter_applications_details')]
+    public function applicationsDetails(int $id, OfferRepository $offerRepository): Response
+    {
+        $offer = $offerRepository->find($id);
+        //dd($offer);
+        return $this->render('recruiter/showApplications.html.twig', [
+            'offer' => $offer,
         ]);
     }
 }
