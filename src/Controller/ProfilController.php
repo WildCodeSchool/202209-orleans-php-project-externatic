5be429ce5a786fd6b7d9fdad674b3d8f1d8d6e74
hhertout <hugues.hertout@gmail.com>
2023-01-20 11:02:36 +0100

    adding securization

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,9 +6,11 @@ use App\Entity\Candidate;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class ProfilController extends AbstractController
 {
+    #[IsGranted("ROLE_RECRUITER")]
     #[Route('/profil/{id}', name: 'app_profil_candidate')]
     public function showProfil(Candidate $candidate): Response
     {
