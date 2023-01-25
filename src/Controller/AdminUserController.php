354a9b948c84e357ea13bbefda2c883d79e1effc
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-01-25 12:08:22 +0100

    FIX changing roles

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -9,7 +9,6 @@ use App\Form\AdminUserType;
 use App\Repository\UserRepository;
 use App\Repository\CandidateRepository;
 use App\Repository\RecruiterRepository;
-use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
